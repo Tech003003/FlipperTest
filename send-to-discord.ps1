88a5524c84408c10598c6012460e85bc2f6796bb
Tech <killyourself11371919@gmail.com>
2025-10-16 21:38:22 -0500

    Update send-to-discord.ps1

--- send-to-discord.ps1
+++ send-to-discord.ps1
@@ -1,9 +1,17 @@
-param(
-    [Parameter(Mandatory = $true)]
-    [string]$FilePath,
+# Send the contents of a text file to a Discord webhook.
+# Update $FilePath below to point to the text file you want to send. Once set,
+# you can simply run the script without any arguments and it will immediately
+# post the file's contents to the configured webhook.
 
+param(
     [string]$WebhookUrl = 'https://discord.com/api/webhooks/1194215544342196275/dgFll7XP-mLSiNWHxUoFkSpuKT62Uf5GN-_IlcuB4VknzWky9UwAlPoQRezxzLoIWRJI'
 )
+
+$FilePath = 'C:\path\to\message.txt'
+
+if (-not $FilePath) {
+    throw 'Update $FilePath in the script before running it.'
+}
 
 if (-not (Test-Path -Path $FilePath -PathType Leaf)) {
     throw "File not found: $FilePath"
